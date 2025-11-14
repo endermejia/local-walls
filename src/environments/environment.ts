191aba0efc47e670576ff7840520fb75317a560c
endermejia <endermejia032@gmail.com>
2025-11-14 16:37:56 +0100

    feat: update environment with anonKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,8 @@
  */
 
 // Supabase project URL (e.g., https://xyzcompany.supabase.co)
-export const ENV_SUPABASE_URL = '';
+export const ENV_SUPABASE_URL = 'https://eswbehepsagigvlytfxv.supabase.co';
 
 // Supabase anonymous key (public anon key)
-export const ENV_SUPABASE_ANON_KEY = '';
+export const ENV_SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzd2JlaGVwc2FnaWd2bHl0Znh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NzM4NzcsImV4cCI6MjA3MzQ0OTg3N30.Rp0_c3r0A2viobLtn4uvFmwLsZjqYBXnbcDZDr9oVig';
