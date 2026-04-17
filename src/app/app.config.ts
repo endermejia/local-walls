67b0da11ec06e6b6ca64036c82883742269097b1
Gabri Mejía <endermejia032@gmail.com>
2026-04-17 09:04:54 +0200

    feat: provide events plugins

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,7 +25,7 @@ import {
 } from '@angular/platform-browser';
 
 import { provideTaiga } from '@taiga-ui/core';
-import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
 import { TUI_LANGUAGE } from '@taiga-ui/i18n';
 
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
@@ -71,7 +71,7 @@ export const appConfig: ApplicationConfig = {
       defaultLanguage: 'es',
     }),
     provideTaiga(),
-    NG_EVENT_PLUGINS,
+    provideEventPlugins(),
     {
       provide: TUI_LANGUAGE,
       useFactory: (global: GlobalData) => global.tuiLanguage,
